a582d68dcc6ea74796ac495b840af090a43605f9
camariu <amariutei1998@gmail.com>
2023-11-22 17:13:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import { Box, useTheme } from '@mui/material';
+
 import Header from './Header/Header';
-import Hero from './Hero/Hero';
+ 
 
 export const App = () => {
-  const theme = useTheme();
-
+ 
   return (
     <div>
       <Header></Header>
